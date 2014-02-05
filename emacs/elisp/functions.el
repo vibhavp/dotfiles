2c0e7d8ddf2e4cfd39b16ae0d42cc5def07a7c93
Vibhav Pant <vibhavp@inspiron-1150.lan>
2014-02-05 23:30:05 +0530

    Add function eval-and-append.

--- emacs/elisp/functions.el
+++ emacs/elisp/functions.el
@@ -3,6 +3,7 @@
   (let ((a (point)))
     (shell-command-on-region (point-min) (point-max) "/home/vibhav/.emacs.d/personal/prettyprint.py" t)
     (goto-char a)))
+
 ;;https://github.com/magnars/.emacs.d/blob/master/defuns/lisp-defuns.el
 (defun eval-and-replace ()
   "Replace the preceding sexp with its value."
@@ -13,3 +14,16 @@
              (current-buffer))
     (error (message "Invalid expression")
            (insert (current-kill 0)))))
+
+(defun eval-and-append ()
+  "Append the preceding sexp's value in a newline"
+  (interactive)
+  (let ((end (point))
+	(start nil))
+    (backward-sexp)
+    (setq start (point))
+    (goto-char end)
+    (eval-and-replace)
+    (goto-char start)
+    (yank)
+    (insert "\n")))
