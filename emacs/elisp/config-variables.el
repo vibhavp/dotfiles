75c80dca191ff4a610ce189c0d46ee6b64a568f4
Vibhav Pant <vibhavp@gmail.com>
2014-02-22 21:36:08 +0530

    Disable initial buffer choice.

--- emacs/elisp/config-variables.el
+++ emacs/elisp/config-variables.el
@@ -1,4 +1,4 @@
-(setq initial-buffer-choice "~/.emacs.d/todo.org")
+;;(setq initial-buffer-choice "~/.emacs.d/todo.org")
 (setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2013/bin/i386-linux"))
 (setq exec-path (append exec-path '("/usr/local/texlive/2013/bin/i386-linux")))
 (setq doc-view-continuous t)
