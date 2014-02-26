209426682f383d31dc5216904b69398f54825240
Vibhav Pant <vibhavp@gmail.com>
2014-02-26 21:22:49 +0530

    Add elisp and subdirectories to load-path

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,6 @@
 (package-initialize)
-(add-to-list 'load-path "~/.emacs.d/elisp/")
+(let ((default-directory "~/.emacs.d/elisp/"))
+  (normal-top-level-add-subdirs-to-load-path))
 
 (require 'config-packages)
 (require 'config-addons)
