8fb1c077d4fd9c9606b4b045348974b6e159e8ea
Vibhav Pant <vibhavp@gmail.com>
2014-03-02 12:25:27 +0530

    Add emms stuff.

--- emacs/elisp/config/config-packages.el
+++ emacs/elisp/config/config-packages.el
@@ -30,4 +30,17 @@
 (package-install 'scheme-complete)
 (package-install 'all)
 
+;; install el-get
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (goto-char (point-max))
+    (eval-print-last-sexp)))
+
+(setq el-get-user-package-directory "~/.emacs.d/elisp/config/")
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+(el-get 'sync)
 (provide 'config-packages)
