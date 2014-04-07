d0ded2c77b4f8b4b3b65dc99f8de6d2fbd4e6f98
Vibhav Pant <vibhavp@gmail.com>
2014-04-07 22:56:43 +0530

    Link new files.

--- firstrun.sh
+++ firstrun.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-emacs -l $(pwd)/emacs/firstrun.el
+emacs -l ./emacs/firstrun.el
 mkdir ~/.emacs.d/elisp
-ln -s $(pwd)/emacs/elisp/config ~/.emacs.d/elisp/config
-ln -s $(pwd)/emacs/init.el ~/.emacs.d/init.el
+mkdir ~/.emacs.d/elisp/lisp
+ln -s ./emacs/elisp/config ~/.emacs.d/elisp/config
+ln -s ./emacs/init.el ~/.emacs.d/init.el
 
-ln -s $(pwd)dunstrc ~/.dunstrc
-
-ln -s $(pwd)xinitrc ~/.xinitrc
+ln -s ./scripts/ ~/scripts
+ln -s ./dunstrc ~/.dunstrc
+ln -s ./xinitrc ~/.xinitrc
+ln -s ./xprofile ~/.xprofile
+chmod +x ~/.xprofile
+ln -s ./linopenrc ~/.linopenrc
