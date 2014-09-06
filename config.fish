cb6e7c42fcc7e7d525b2f77f17379b2a32074954
vibhav Pant <vibhavp@gmail.com>
2014-09-06 13:01:27 +0530

    Initial Commit.

---
+++ config.fish
@@ -0,0 +1,10 @@
+set -x LC_ALL en_GB.UTF-8
+set -x LC_CTYPE en_GB.UTF-8
+set -gx INFOPATH /usr/local/texlive/2014/texmf-dist/doc/info $INFOPATH
+set -gx MANPATH /usr/local/texlive/2014/texmf-dist/doc/man $MANPATH
+set -gx PATH /home/vibhavp/dotfiles/scripts/ /usr/lib/ccache/bin/ /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/core_perl/ /usr/local/texlive/2014/bin/i386-linux
+
+function fish_title
+  true
+end
+
