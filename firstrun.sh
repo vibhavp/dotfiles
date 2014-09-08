35631a16a07ac026784e9bd1ae942189d6b907c8
vibhav Pant <vibhavp@gmail.com>
2014-09-08 19:15:54 +0530

    Too lazy to add message.

--- firstrun.sh
+++ firstrun.sh
@@ -1,20 +1,25 @@
 #!/bin/sh
 
-emacs -l $(pwd)/emacs/firstrun.el
+emacs -l $(dirname $0)/emacs/firstrun.el
 mkdir ~/.emacs.d/elisp
 mkdir ~/.emacs.d/elisp/lisp
-ln -sf $(pwd)/emacs/elisp/config ~/.emacs.d/elisp/config
-ln -sf $(pwd)/emacs/init.el ~/.emacs.d/init.el
+ln -sf $(dirname $0)/emacs/elisp/config ~/.emacs.d/elisp/config
+ln -sf $(dirname $0)/emacs/init.el ~/.emacs.d/init.el
 
-ln -sf $(pwd)/dunstrc ~/.dunstrc
-ln -sf $(pwd)/config.fish ~/.config/fish/config.fish
-ln -sf $(pwd)/youtube-dl.conf ~/.youtube-dl.conf
-ln -sf $(pwd)/linopenrc ~/.linopenrc
+ln -sf $(dirname $0)/dunstrc ~/.dunstrc
+ln -sf $(dirname $0)/config.fish ~/.config/fish/config.fish
+ln -sf $(dirname $0)/youtube-dl.conf ~/.youtube-dl.conf
+ln -sf $(dirname $0)/linopenrc ~/.linopenrc
 
-ln -sf $(pwd)/x11/xinitrc ~/.xinitrc
-ln -sf $(pwd)/x11/xprofile ~/.xprofile
-ln -sf $(pwd)/x11/Xmodmap ~/.Xmodmap
-ln -sf $(pwd)/x11/Xresources ~/.Xresources
-chmod +x $(pwd)/x11/xprofile
+ln -sf $(dirname $0)/x11/xinitrc ~/.xinitrc
+ln -sf $(dirname $0)/x11/xprofile ~/.xprofile
+ln -sf $(dirname $0)/x11/Xmodmap ~/.Xmodmap
+ln -sf $(dirname $0)/x11/Xresources ~/.Xresources
+chmod +x $(dirname $0)/x11/xprofile
 
-chmod +x $(pwd)/scripts/*
+ln -sf $(dirname $0)/dwb/custom_keys ~/.config/dwb/default/custom_keys
+ln -sf $(dirname $0)/dwb/settings ~/.config/dwb/settings
+
+chmod +x $(dirname $0)/scripts/*
+
+exec $(dirname $0)/scripts/update-scripts
