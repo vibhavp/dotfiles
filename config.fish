f9afbda9068b4108be7a8bff462fe6b3afb2af11
vibhav Pant <vibhavp@gmail.com>
2014-09-09 14:24:51 +0530

    Add /bin to PATH.

--- config.fish
+++ config.fish
@@ -2,7 +2,7 @@ set -x LC_ALL en_GB.UTF-8
 set -x LC_CTYPE en_GB.UTF-8
 set -gx INFOPATH /usr/local/texlive/2014/texmf-dist/doc/info $INFOPATH
 set -gx MANPATH /usr/local/texlive/2014/texmf-dist/doc/man $MANPATH
-set -gx PATH /home/vibhavp/dotfiles/scripts/ /usr/lib/ccache/bin/ /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/core_perl/ /usr/local/texlive/2014/bin/i386-linux
+set -gx PATH /home/vibhavp/dotfiles/scripts/ /usr/lib/ccache/bin/ /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/core_perl/ /usr/local/texlive/2014/bin/i386-linux /bin/
 
 function fish_title
   true
