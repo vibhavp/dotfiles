83adc20ee17b061a494f6524ab41c5897bc45ac0
vibhav Pant <vibhavp@gmail.com>
2014-09-10 18:10:58 +0530

    Cleanup.

--- emacs/elisp/config/config-lisp.el
+++ emacs/elisp/config/config-lisp.el
@@ -11,17 +11,6 @@
 (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
 (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
 
-;;Enable auto-complete
-;;(add-to-list 'load-path "/home/vibhavp/.emacs.d/auto-complete")
-;;(require 'auto-complete-config)
-;;(add-to-list 'ac-dictionary-directories "/home/vibhavp/.emacs.d/auto-complete/ac-dict")
-;;(ac-config-default)
-
-;;load SLIME
-;;(load (expand-file-name "~/quicklisp/slime-helper.el"))
-;; Replace "sbcl" with the path to your implementation
-(setq inferior-lisp-program "sbcl")
-
 ;; auto compile on save
 (require 'auto-compile)
 (auto-compile-on-save-mode)
