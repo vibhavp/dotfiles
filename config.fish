a2b814ffd376e9288d38b9c080a5a2ece6806305
vibhav Pant <vibhavp@gmail.com>
2014-09-10 18:08:19 +0530

    Remove emclient script, use alias.

--- config.fish
+++ config.fish
@@ -4,6 +4,7 @@ set -gx INFOPATH /usr/local/texlive/2014/texmf-dist/doc/info $INFOPATH
 set -gx MANPATH /usr/local/texlive/2014/texmf-dist/doc/man $MANPATH
 set -gx PATH /home/vibhavp/dotfiles/scripts/ /usr/lib/ccache/bin/ /usr/local/sbin /usr/local/bin /usr/bin /usr/bin/core_perl/ /usr/local/texlive/2014/bin/i386-linux /bin/
 
+alias emclient "emacsclient -c"
 function fish_title
   true
 end
