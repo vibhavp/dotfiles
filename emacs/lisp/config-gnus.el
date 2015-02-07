a475811340e43b0cd27ca4090d466dc6e74c9da0
Vibhav Pant <vibhavp@gmail.com>
2015-02-07 12:36:23 +0530

    add packages.

--- emacs/lisp/config-gnus.el
+++ emacs/lisp/config-gnus.el
@@ -1,7 +1,18 @@
-(setq gnus-select-method 
+(setq gnus-select-method
       '(nnmaildir "GMail" 
                   (directory "~/Maildir/")
                   (directory-files nnheader-directory-files-safe) 
-                  (get-new-mail nil)))
+                  (get-new-mail nil))
+ message-send-mail-function 'smtpmail-send-it
+ smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+ smtpmail-auth-credentials '(("smtp.gmail.com" 587
+			      "user@gmail.com" nil))
+ smtpmail-default-smtp-server "smtp.gmail.com"
+ smtpmail-smtp-server "smtp.gmail.com"
+ smtpmail-smtp-service 587
+ gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
+ gnus-thread-sort-functions '(gnus-thread-sort-by-number
+			      gnus-thread-sort-by-date)
+ mail-host-address "gmail.com")
 
 (provide 'config-gnus)
--- emacs/lisp/config-variables.el
+++ emacs/lisp/config-variables.el
@@ -1,6 +1,6 @@
 ;;(setq initial-buffer-choice "~/.emacs.d/todo.org")
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2013/bin/i386-linux"))
-(setq exec-path (append exec-path '("/usr/local/texlive/2013/bin/i386-linux")))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2014/bin/x86_64-linux"))
+(setq exec-path (append exec-path '("/usr/local/texlive/2014/bin/x86_64-linux")))
 (setq doc-view-continuous t)
 (setq vc-follow-symlinks t)
 
