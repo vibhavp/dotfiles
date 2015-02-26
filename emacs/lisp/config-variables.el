28beed6ca2895dfa820455c9ec215ded183883d5
Vibhav Pant <vibhavp@gmail.com>
2015-02-26 13:22:17 +0530

    Remove PATH.

--- emacs/lisp/config-variables.el
+++ emacs/lisp/config-variables.el
@@ -1,6 +1,4 @@
 ;;(setq initial-buffer-choice "~/.emacs.d/todo.org")
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/texlive/2014/bin/x86_64-linux"))
-(setq exec-path (append exec-path '("/usr/local/texlive/2014/bin/x86_64-linux")))
 (setq doc-view-continuous t)
 (setq vc-follow-symlinks t)
 
