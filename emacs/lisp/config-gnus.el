5a106d0d43e65674f1cfa8e3959c669499450a45
Vibhav Pant <vibhavp@gmail.com>
2015-03-24 17:57:39 +0530

    A better format.

--- emacs/lisp/config-gnus.el
+++ emacs/lisp/config-gnus.el
@@ -1,18 +1,24 @@
 (setq gnus-select-method
       '(nnmaildir "GMail" 
-                  (directory "~/Maildir/")
-                  (directory-files nnheader-directory-files-safe) 
-                  (get-new-mail nil))
- message-send-mail-function 'smtpmail-send-it
- smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
- smtpmail-auth-credentials '(("smtp.gmail.com" 587
-			      "user@gmail.com" nil))
- smtpmail-default-smtp-server "smtp.gmail.com"
- smtpmail-smtp-server "smtp.gmail.com"
- smtpmail-smtp-service 587
- gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
- gnus-thread-sort-functions '(gnus-thread-sort-by-number
-			      gnus-thread-sort-by-date)
- mail-host-address "gmail.com")
-
+		  (directory "~/Maildir/")
+		  (directory-files nnheader-directory-files-safe) 
+		  (get-new-mail nil))
+      message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587
+				   "user@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
+      gnus-thread-sort-functions '(gnus-thread-sort-by-number
+				   gnus-thread-sort-by-date)
+      mail-host-address "gmail.com")
+(setq gnus-summary-line-format "%U%R%z%B%(%[%4&user-date;: %-23,23f%]%) %s\n"
+      gnus-sum-thread-tree-false-root ""
+      gnus-sum-thread-tree-indent " "
+      gnus-sum-thread-tree-leaf-with-other "├► "
+      gnus-sum-thread-tree-root ""
+      gnus-sum-thread-tree-single-leaf "╰► "
+      gnus-sum-thread-tree-vertical "│")
 (provide 'config-gnus)
