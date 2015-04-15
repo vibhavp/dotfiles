48f7da9d2a36332d46af717d4f706d6c11d9d421
Vibhav Pant <vibhavp@gmail.com>
2015-04-15 11:45:23 +0530

    Enable flycheck.

--- emacs/lisp/config-c.el
+++ emacs/lisp/config-c.el
@@ -20,9 +20,7 @@
                              (or (getenv "CFLAGS") "-g")
                              file))))))
 (electric-pair-mode)
-(add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
-(add-hook 'objc-mode-hook 'irony-mode)
 (defun my-irony-mode-hook ()
   (define-key irony-mode-map [remap completion-at-point]
     'irony-completion-at-point-async)
@@ -30,5 +28,5 @@
     'irony-completion-at-point-async))
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
 (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
+(global-flycheck-mode t)
 (provide 'config-c)
