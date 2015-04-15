b610f015e0ef833bf679332b5c6b67b9186c7c15
Vibhav Pant <vibhavp@gmail.com>
2015-04-15 11:45:12 +0530

    change backup file loction.

--- emacs/lisp/config-addons.el
+++ emacs/lisp/config-addons.el
@@ -1,10 +1,22 @@
 (defvar config-recentf-mode-enabled nil)
-(eval-after-load 'tramp '(setenv "SHELL" "/bin/bash"))
+(eval-after-load 'tramp '(setenv "SHELL" "/bin/zsh"))
 
 ;;undo tree
 (global-undo-tree-mode 1)
 
 ;; set browser
-(setq browse-url-browser-function 'browse-url-generic
-      browse-url-generic-program "conkeror")
+(setq browse-url-browser-function 'browse-url-chromium)
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup/"))
+      version-control t
+      delete-old-versions t
+      kept-new-versions 6
+      kept-old-versions 2
+      backup-by-copying t)
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
+(setq initial-buffer-choice 'eshell)
+(require 'projectile)
+(projectile-global-mode t)
+(setq projectile-indexing-method 'alien
+      projectile-enable-caching t)
 (provide 'config-addons)
