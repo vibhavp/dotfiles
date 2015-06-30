b877d1a8cc10138158d6e465292afcc3cb37990e
Vibhav Pant <vibhavp@gmail.com>
2015-06-30 22:48:28 +0530

    use xfce4 terminal

--- emacs/lisp/config-c.el
+++ emacs/lisp/config-c.el
@@ -20,13 +20,16 @@
                              (or (getenv "CFLAGS") "-g")
                              file))))))
 (electric-pair-mode)
+(add-hook 'c++-mode-hook 'irony-mode)
 (add-hook 'c-mode-hook 'irony-mode)
+(add-hook 'objc-mode-hook 'irony-mode)
 (defun my-irony-mode-hook ()
   (define-key irony-mode-map [remap completion-at-point]
     'irony-completion-at-point-async)
   (define-key irony-mode-map [remap complete-symbol]
     'irony-completion-at-point-async))
 (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-(add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+(when (eq system-type 'windows-nt)
+  (setq w32-pipe-read-delay 0))
 (global-flycheck-mode t)
 (provide 'config-c)
