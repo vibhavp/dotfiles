a14b5f993eaa528a5179761b65c0bcf4e6d9c7ff
Vibhav Pant <vibhavp@gmail.com>
2016-12-13 17:25:59 +0530

    Add flycheck

--- emacs/lisp/config-js.el
+++ emacs/lisp/config-js.el
@@ -1,6 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js-mode-hook 'js2-minor-mode)
-
+(require 'flycheck)
 (defun use-eslint-from-node-modules ()
   (let* ((root (locate-dominating-file
                 (or (buffer-file-name) default-directory)
@@ -12,5 +12,10 @@
 	(if (file-executable-p eslint)
 	    (setq-local flycheck-javascript-eslint-executable eslint)
 	  (setq-local flycheck-javascript-eslint-executable (format "node %s" eslint))))))
-(add-hook 'js2-mode-hook 'use-eslint-from-node-modules)
+;;(add-hook 'js2-mode-hook 'use-eslint-from-node-modules)
+(flycheck-add-mode 'javascript-eslint 'js2-mode)
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(json-jsonlist)))
+
 (provide 'config-js)
