f8068df71bb2746d9bbcf486a1bfd1a68197143d
Vibhav Pant <vibhavp@gmail.com>
2017-04-07 00:29:25 +0530

    Fix dired config.

--- emacs/lisp/config-dired.el
+++ emacs/lisp/config-dired.el
@@ -1,9 +1,10 @@
 ;; '(' and  ')' for details
 (use-package dired
   :init
-  (setq dired-listing-switches "-aDlh"))
-(use-package dired-async)
-(use-package dired-details+)
-(use-package dired-x)
+  (setq dired-listing-switches "-aDlh")
+  :config
+  (use-package dired-async)
+  (use-package dired-details+)
+  (use-package dired-x))
 
 (provide 'config-dired)
