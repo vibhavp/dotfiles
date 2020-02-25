11187855c57660a22dc4469b444256c18b167f1d
Vibhav Pant <vibhavp@gmail.com>
2020-02-25 11:27:27 +0530

    Use all-the-icons

--- emacs/lisp/config-dired.el
+++ emacs/lisp/config-dired.el
@@ -15,4 +15,8 @@
   :after dired
   :hook ((dired-mode . dired-omit-mode)))
 
+(use-package all-the-icons-dired
+  :ensure t
+  :hook ((dired-mode . all-the-icons-dired-mode)))
+
 (provide 'config-dired)
