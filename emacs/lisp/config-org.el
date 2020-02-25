0a32bc90f919f3f8b8edc2d1ca7f5839ae0f48ec
Vibhav Pant <vibhavp@gmail.com>
2020-02-25 11:26:47 +0530

    Add org config

---
+++ emacs/lisp/config-org.el
@@ -0,0 +1,14 @@
+(use-package org
+  :config
+  (setq org-latex-default-packages-alist
+	(seq-remove (lambda (package) (string-equal (cadr package) "hyperref"))
+		    org-latex-default-packages-alist))
+  (add-to-list 'org-latex-packages-alist
+	       '("colorlinks=true,urlcolor=blue,linkcolor=red" "hyperref" nil)))
+
+(use-package ox-beamer
+  :after (org)
+  :config
+  (setq org-beamer-theme nil))
+
+(provide 'config-org)
