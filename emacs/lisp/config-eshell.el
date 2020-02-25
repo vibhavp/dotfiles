f6909420c928e28d672b42fcd1cf2f623a8d15a9
Vibhav Pant <vibhavp@gmail.com>
2020-02-25 11:28:09 +0530

    Add exec-path-from-shell-variables

--- emacs/lisp/config-eshell.el
+++ emacs/lisp/config-eshell.el
@@ -2,7 +2,9 @@
 
 (use-package exec-path-from-shell
   :init
-  (setq exec-path-from-shell-arguments '("-l"))
+  (setq exec-path-from-shell-arguments '("-l")
+	exec-path-from-shell-variables '("PATH" "MANPATH" "LC_CTYPE" "LC_ALL"
+					 "GO111MODULE" "GOPRIVATE" "GONOSUMDB"))
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize))
