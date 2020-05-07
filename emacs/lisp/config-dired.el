1f0230f7cac9e9ee3ff6e8ab7f2bd7ca30bc2e21
Vibhav Pant <vibhavp@gmail.com>
2020-05-07 12:45:59 +0530

    Add dired-du, use human-readable file sizes in dired.

--- emacs/lisp/config-dired.el
+++ emacs/lisp/config-dired.el
@@ -2,6 +2,8 @@
 
 ;; '(' and  ')' for details
 (use-package dired
+  :config
+  (setq dired-listing-switches "-alh")
   :commands (dired))
 
 (use-package dired-async
@@ -18,5 +20,10 @@
 (use-package all-the-icons-dired
   :ensure t
   :hook ((dired-mode . all-the-icons-dired-mode)))
+
+(use-package dired-du
+  :ensure t
+  :config
+  (setq dired-du-size-format t))
 
 (provide 'config-dired)
