ff2fb462b07605d952f5ab2f5c751d2f8909f056
Vibhav Pant <vibhavp@gmail.com>
2020-12-15 14:58:10 +0530

    Use lsp-format-buffer instead of gofmt-before-save.

--- emacs/lisp/config-go.el
+++ emacs/lisp/config-go.el
@@ -1,7 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package go-mode
-  :hook ((before-save . gofmt-before-save))
   :defines gofmt-command
   :config
   (setq gofmt-command "goimports")
@@ -10,7 +9,8 @@
 (use-package lsp-go
   :demand t
   :hook ((go-mode . lsp)
-	 (go-mode . lsp-lens-mode))
+	 (go-mode . lsp-lens-mode)
+	 (go-mode . (lambda () (add-hook 'before-save-hook #'lsp-format-buffer 0 t))))
   :ensure lsp-mode
   :after (lsp-mode lsp-lens))
 
