683d30f5e3da124e4971f10c002faf6b5293c8ba
Vibhav Pant <vibhavp@gmail.com>
2020-12-15 14:58:45 +0530

    Use rust-analyzer.

--- emacs/lisp/config-rust.el
+++ emacs/lisp/config-rust.el
@@ -4,8 +4,15 @@
 ;; (setq racer-rust-src-path "~/src/rust/src")
 ;; (add-hook 'rust-mode-hook #'racer-mode)
 ;; (add-hook 'racer-mode-hook #'eldoc-mode)
-(use-package rust-mode
-  :hook (((rust-mode) . lsp))
-  :after lsp
-  :ensure t)
+
+(use-package lsp-rust
+  :demand t
+  :config
+  (add-to-list 'lsp-disabled-clients 'rls)
+  :hook ((rust-mode . lsp)
+	 (rust-mode . lsp-lens-mode)
+	 (rust-mode . (lambda () (add-hook 'before-save-hook #'lsp-format-buffer 0 t))))
+  :ensure lsp-mode
+  :after (lsp-mode lsp-lens))
+
 (provide 'config-rust)
