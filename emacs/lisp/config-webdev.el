646cc43af794627cf87fd744d3702efb13397cfb
Vibhav Pant <vibhavp@gmail.com>
2021-05-22 15:06:53 +0530

    Comment out web-dev-mode

--- emacs/lisp/config-webdev.el
+++ emacs/lisp/config-webdev.el
@@ -1,16 +1,16 @@
 ;; -*- lexical-binding: t; -*-
 
-(use-package web-mode
-  :init
-  (setq web-mode-enable-current-element-highlight t
-	web-mode-enable-current-column-highlight t)
-  :mode (("\\.phtml\\'" . web-mode)
-	 ("\\.tpl\\.php\\'" . web-mode)
-	 ("\\.[agj]sp\\'" . web-mode)
-	 ("\\.as[cp]x\\'" . web-mode)
-	 ("\\.erb\\'" . web-mode)
-	 ("\\.mustache\\'" . web-mode)
-	 ("\\.mustache\\'" . web-mode)("\\.djhtml\\'" . web-mode))
-  :ensure t)
+;; (use-package web-mode
+;;   :init
+;;   (setq web-mode-enable-current-element-highlight t
+;; 	web-mode-enable-current-column-highlight t)
+;;   :mode (("\\.phtml\\'" . web-mode)
+;; 	 ("\\.tpl\\.php\\'" . web-mode)
+;; 	 ("\\.[agj]sp\\'" . web-mode)
+;; 	 ("\\.as[cp]x\\'" . web-mode)
+;; 	 ("\\.erb\\'" . web-mode)
+;; 	 ("\\.mustache\\'" . web-mode)
+;; 	 ("\\.mustache\\'" . web-mode)("\\.djhtml\\'" . web-mode))
+;;   :ensure t)
 
 (provide 'config-webdev)
