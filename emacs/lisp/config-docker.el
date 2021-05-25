18f7456f011fa4f17ee6b362f8771ac95f955b27
Vibhav Pant <vibhavp@gmail.com>
2021-05-25 13:10:22 +0530

    Add lsp-dockerfile.

--- emacs/lisp/config-docker.el
+++ emacs/lisp/config-docker.el
@@ -2,4 +2,10 @@
   :ensure t
   :commands (docker))
 
+(use-package lsp-dockerfile
+  :demand t
+  :hook ((dockerfile-mode . lsp-deferred))
+  :after (lsp-mode lsp-lens)
+  :ensure lsp-mode)
+
 (provide 'config-docker)
