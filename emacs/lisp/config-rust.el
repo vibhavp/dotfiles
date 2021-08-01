a0d7e8e2c8351314401008eab2ab428f7213249a
Vibhav Pant <vibhavp@gmail.com>
2021-08-01 21:01:39 +0530

    Use clippy for LSP diagnostics

--- emacs/lisp/config-rust.el
+++ emacs/lisp/config-rust.el
@@ -9,6 +9,9 @@
   :demand t
   :config
   (add-to-list 'lsp-disabled-clients 'rls)
+  (setq lsp-rust-analyzer-cargo-override-command ["cargo" "clippy" "--message-format=json" "--workspace"
+						  "--all-targets" "--" "-Aclippy::upper_case_acronyms"]
+	lsp-rust-analyzer-proc-macro-enable t)
   :hook ((rust-mode . lsp)
 	 (rust-mode . lsp-lens-mode)
 	 (rust-mode . (lambda () (add-hook 'before-save-hook #'lsp-format-buffer 0 t))))
