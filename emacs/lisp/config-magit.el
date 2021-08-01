24f74ccdb2f9fa717e9cfae1e2d1c8debba2ae3c
Vibhav Pant <vibhavp@gmail.com>
2021-08-01 21:01:02 +0530

    Enable magit-delta

--- emacs/lisp/config-magit.el
+++ emacs/lisp/config-magit.el
@@ -10,10 +10,20 @@
   ;; :after magit-todos
   )
 
+(use-package magit-delta
+  :ensure t
+  :hook ((magit-mode . magit-delta-mode)))
+
 (use-package forge
   :ensure t
   :after (magit))
 
-;; (add-hook 'after-init-hook 'magit-todos-mode)
+(use-package libgit
+  :ensure t)
+
+(use-package magit-libgit
+  :ensure t)
+
+;; (Add-Hook 'after-init-hook 'magit-todos-mode)
 
 (provide 'config-magit)
