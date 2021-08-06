f3de386999d66cc110eb602074b9c7d44e47fcca
Vibhav Pant <vibhavp@vibhavp-mbp.localdomain>
2021-08-06 13:02:57 +0530

    Use the correct melpa URL.

--- emacs/firstrun.el
+++ emacs/firstrun.el
@@ -1,6 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/"))
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 (package-refresh-contents)
 (package-install 'use-package)
