3f05eee912662a54935b281e5b83c587e9bc36e7
Vibhav Pant <vibhavp@gmail.com>
2022-01-27 12:42:08 +0530

    Add lsp-yaml

--- emacs/lisp/config-yaml.el
+++ emacs/lisp/config-yaml.el
@@ -2,7 +2,17 @@
 
 (use-package yaml-mode
   :ensure t
-  :hook ((yaml-mode . highlight-indentation-mode))
+  :hook ((yaml-mode . highlight-indentation-mode)
+	 (yaml-mode . lsp)
+	 (yaml-mode . (lambda ()
+			(add-hook 'before-save-hook #'lsp-format-buffer 0 t))))
   :mode ("\\.\\(e?ya?\\|ra\\)ml\\'" . yaml-mode))
+
+(use-package lsp-yaml
+  :demand t
+  :ensure lsp-mode
+  :init
+  (setq lsp-yaml-server-command '("npx" "--yes" "yaml-language-server" "--stdio"))
+  :after (lsp-mode yaml-mode))
 
 (provide 'config-yaml)
