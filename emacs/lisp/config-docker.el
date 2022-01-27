cf82cc184e0dd8d1b80cbb99be0a2e65a1bc0e61
Vibhav Pant <vibhavp@gmail.com>
2022-01-27 12:39:52 +0530

    Use npx for dockerfile-ls.

--- emacs/lisp/config-docker.el
+++ emacs/lisp/config-docker.el
@@ -6,6 +6,8 @@
   :demand t
   :hook ((dockerfile-mode . lsp-deferred))
   :after (lsp-mode lsp-lens)
+  :init
+  (setq lsp-dockerfile-language-server-command '("npx" "--yes" "dockerfile-language-server-nodejs" "--stdio"))
   :ensure lsp-mode)
 
 (provide 'config-docker)
