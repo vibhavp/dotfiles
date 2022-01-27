8da7f340296fdb2618891db5018e902b4cf5a47a
Vibhav Pant <vibhavp@gmail.com>
2022-01-27 12:41:11 +0530

    Use npx for language servers.

--- emacs/lisp/config-js.el
+++ emacs/lisp/config-js.el
@@ -7,9 +7,10 @@
   ;; :mode ("\\.js\\'" . js-mode)
   :interpreter ("node" . js-mode)
   :hook (;; (js-mode . js2-minor-mode)
-	 ;; (js-mode . prettier-js-mode)
+	 (js-mode . prettier-js-mode)
 	 ;; (js-mode . (lambda () (add-hook 'before-save-hook #'whitespace-cleanup 0 t)))
-	 (js-mode . (lambda () (add-hook 'before-save-hook #'lsp-format-buffer 0 t))))
+	 ;; (js-mode . (lambda () (add-hook 'before-save-hook #'lsp-format-buffer 0 t)))
+	 )
   :ensure t)
 
 (use-package json-mode
@@ -26,6 +27,27 @@
   :hook ((js-mode . lsp)
 	 (typescript-mode . lsp))
   :ensure lsp-mode
+  :init
+  (setq lsp-clients-typescript-tls-path (expand-file-name "~/bin/lsp-tsls"))
+  (setq lsp-clients-typescript-init-opts `(:includeCompletionsForImportStatements
+						    ,(lsp-json-bool t)))
+  :config
+  (lsp-dependency 'typescript-language-server
+		  '(:system lsp-clients-typescript-tls-path))
+  (lsp-dependency 'typescript
+                  `(:system ,(f-join (map-nested-elt
+				      (json-parse-string
+				       (shell-command-to-string "npm ll -g typescript --json"))
+				      '("dependencies" "typescript" "path"))
+				     "lib")))
   :after (lsp-mode typescript-mode))
+
+(use-package lsp-eslint
+  :demand t
+  :ensure lsp-mode
+  :init
+  (setq lsp-eslint-server-command `("node" ,(expand-file-name "~/bin/vscode-eslint/server/out/eslintServer.js") "--stdio")
+	lsp-eslint-enable t)
+  :after (lsp-javascript))
 
 (provide 'config-js)
