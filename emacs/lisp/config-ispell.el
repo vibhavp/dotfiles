42fee95cc360ad9ae2ba7ca9b6e5913e0e094735
Vibhav Pant <vibhavp@gmail.com>
2022-01-27 12:40:48 +0530

    Disable flyspell on tramp

--- emacs/lisp/config-ispell.el
+++ emacs/lisp/config-ispell.el
@@ -1,16 +1,22 @@
 ;; -*- lexical-binding: t; -*-
 
+(defun config-ispell-flyspell-if-not-tramp ()
+  (unless (file-remote-p default-directory) (flyspell-mode)))
+
 (use-package ispell
   :init
   (setq ispell-current-dictionary "~/usr/share/hunspell/en-GB"
-	ispell-program-name "ispell"))
+	ispell-program-name "ispell")
+  :demand t)
 
 (use-package flyspell
   :after ispell
   :init
   (setq flyspell-issue-message-flag nil)
-  :hook ((text-mode . flyspell-mode)
-	 (change-log-mode-hook . flyspell-mode)
-	 (log-edit-mode-hook . flyspell-mode)))
+  :hook ((text-mode . config-ispell-flyspell-if-not-tramp)
+	 (change-log-mode-hook . config-ispell-flyspell-if-not-tramp)
+	 (log-edit-mode-hook . config-ispell-flyspell-if-not-tramp)))
+
+
 
 (provide 'config-ispell)
