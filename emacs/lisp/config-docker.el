022e9ab02ac242d720ca35be7705c2d4d1ce1689
Vibhav Pant <vibhavp@gmail.com>
2022-06-03 15:11:51 +0530

    Add package kubernetes.

--- emacs/lisp/config-docker.el
+++ emacs/lisp/config-docker.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t; -*-
 (use-package docker
   :ensure t
   :commands (docker))
@@ -9,5 +10,28 @@
   :init
   (setq lsp-dockerfile-language-server-command '("npx" "--yes" "dockerfile-language-server-nodejs" "--stdio"))
   :ensure lsp-mode)
+
+(use-package kubernetes
+  :ensure t
+  :init
+  (defmacro kubernetes-utils--save-window-state (&rest body)
+  "Restore window state after executing BODY.
+
+This is useful if the buffer is erased and repopulated in BODY,
+in which case `save-excursion' is insufficient to restore the
+window state."
+  `(let ((pos (point))
+         (col (current-column))
+         (window-start-line (window-start))
+         (inhibit-redisplay t))
+     (save-excursion
+       ,@body)
+     (goto-char pos)
+     (move-to-column col)
+     (set-window-start (selected-window) window-start-line)))
+  :commands (kubernetes-overview)
+  :config
+  (setq kubernetes-poll-frequency 3600
+        kubernetes-redraw-frequency 3600))
 
 (provide 'config-docker)
