9c7b287c0c058eb3c6824e7c79c4352ca2f9edf7
Vibhav Pant <vibhavp@gmail.com>
2022-06-03 15:11:40 +0530

    Disable google-c-style hooks.

--- emacs/lisp/config-c.el
+++ emacs/lisp/config-c.el
@@ -3,11 +3,7 @@
 ;;Everything related to C
 
 (use-package google-c-style
-  :demand t
-  :hook
-  ((c-mode-common . google-set-c-style)
-   (c-mode-common . google-make-newline-indent))
-  :ensure t)
+  :demand t)
 
 ;; (use-package flycheck
 ;;   :config
@@ -18,7 +14,14 @@
   :demand t
   :after lsp-mode
   :ensure lsp-mode
-  :hook (((c-mode c++-mode objc-mode) . lsp)))
+  :hook (((c-mode c++-mode objc-mode) . lsp)
+	 ((c-mode-common objc-mode) .
+	  (lambda ()
+	    (remove-hook 'flymake-diagnostic-functions 'flymake-cc t)
+	    (add-hook 'before-save-hook #'delete-trailing-whitespace 0 t)))
+	 ((flymake-mode) .
+	  (lambda ()
+	    (setq-local flymake-proc-allowed-file-name-masks nil)))))
 
 ;; (use-package ccls
 ;;   :demand t
