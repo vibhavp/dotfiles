1754ed5b7aca7265d764d26d32d388b7e9671ce3
Vibhav Pant <vibhavp@gmail.com>
2024-02-24 14:07:21 +0530

    Update lsp-clangd-binary-path.

--- emacs/lisp/config-c.el
+++ emacs/lisp/config-c.el
@@ -5,19 +5,29 @@
 (use-package lsp-clangd
   :after (lsp-mode cc-mode)
   :ensure lsp-mode
+  :init
+  (setq lsp-clangd-binary-path (or
+				(executable-find "clangd-17")
+				(executable-find "clangd-15")
+				(executable-find "clangd")))
   :hook (((c-mode c++-mode objc-mode) . lsp-deferred)
 	 ((c-mode-common objc-mode) .
 	  (lambda ()
-	    (electric-indent-local-mode -1)
 	    (remove-hook 'flymake-diagnostic-functions 'flymake-cc t)))))
 
 (use-package c-ts-mode
   :if (treesit-available-p)
   :init
+  (setq c-ts-mode-emacs-sources-support t)
   (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
   (add-to-list 'lsp-language-id-configuration '(c-ts-mode . "c"))
   (add-to-list 'lsp-language-id-configuration '(c++-ts-mode . "cpp"))
+  (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
+  (add-to-list 'major-mode-remap-alist '(c-or-c++-mode . c-or-c++-ts-mode))
   :hook (((c-ts-base-mode) . lsp-deferred)))
+
+(use-package objc-mode
+  :mode "\\.mm\\'")
 
 ;; (use-package ccls
 ;;   :demand t
